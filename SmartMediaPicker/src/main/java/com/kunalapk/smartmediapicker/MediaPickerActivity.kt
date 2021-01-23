5879d8cf2d6d2d782b9047d4a71e7559c7dc0b27
kunalapk <kunalpasricha7@gmail.com>
2021-01-23 16:45:50 +0530

    added smartrecyclerview

---
+++ SmartMediaPicker/src/main/java/com/kunalapk/smartmediapicker/MediaPickerActivity.kt
@@ -0,0 +1,14 @@
+package com.kunalapk.smartmediapicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MediaPickerActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_picker)
+
+
+    }
+}
